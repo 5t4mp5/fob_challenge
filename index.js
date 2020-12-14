e115c83c2e0f7ec04bf02fbb21205905d0f4ddd3
Dave <dave.e.siegel@gmail.com>
2020-12-14 12:02:06 -0500

    error displays

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const fetchData = async () => {
   return data;
 };
 
-const selected = { product, country, state };
+const selected = { product: undefined, country: undefined, state: undefined };
+const fetchedData = {
+  products: [],
+  countries: [],
+  states: [],
+};
 
 const render = async () => {
-  const { states, countries, products } = await fetchData();
+  await fetchData()
+    .then((data) => {
+      (fetchedData.products = data.products),
+        (fetchedData.countries = data.countries),
+        (fetchedData.states = data.states);
+    })
+    .catch((e) => console.error(e));
+  const { products, countries, states } = fetchedData;
   const form = document.querySelector('form');
   const productSelect = document.querySelector('#product-select');
+  productSelect.addEventListener('change', onProductChange);
   products.forEach((product) => {
     productSelect.innerHTML += `<option value=${product.product_id}>${product.product_name}</option>`;
   });
@@ -17,39 +30,58 @@ const render = async () => {
   countries.forEach((country) => {
     countrySelect.innerHTML += `<option value=${country.country_id}>${country.name}</option>`;
   });
-  countrySelect.addEventListener('change', (evt) =>
-    onCountryChange(evt, form, states)
-  );
+  countrySelect.addEventListener('change', (evt) => onCountryChange(evt, form));
+  stateSelect = document.createElement('select');
+  stateSelect.setAttribute('id', 'state-select');
+  stateSelect.addEventListener('change', onStateChange);
+  form.appendChild(stateSelect);
+  stateSelect.innerHTML =
+    '<option value="">--Please Select Your State--</option>';
 };
 
-function onCountryChange(evt, form, states) {
-  const countryId = evt.target.value;
-  selected.country = evt.target.value.length ?
-  if (evt.target.value.length) {
-    const stateOptions = states.filter(
-      (state) => state.country_id === evt.target.value
-    );
-    let stateSelect = document.querySelector('#state-select');
-    if (!stateSelect) {
-      stateSelect = document.createElement('select');
-      stateSelect.setAttribute('id', 'state-select');
-      form.appendChild(stateSelect);
-    }
-    stateSelect.innerHTML = stateOptions.reduce((options, state) => {
-      return options + `<option value=${state.state_id}>${state.name}</option>`;
-    }, '<option value="">--Please Select Your State--</option>');
-  }
+function onCountryChange(evt) {
+  selected.country = fetchedData.countries.find(
+    (country) => country.country_id === evt.target.value
+  );
+  const stateOptions = fetchedData.states.filter(
+    (state) => state.country_id === evt.target.value
+  );
+  let stateSelect = document.querySelector('#state-select');
+  stateSelect.innerHTML = stateOptions.reduce((options, state) => {
+    return options + `<option value=${state.state_id}>${state.name}</option>`;
+  }, '<option value="">--Please Select Your State--</option>');
+
+  checkShippable();
 }
 
-function checkShippable(productId, countryId, stateId){
-  const productStatus = productId ? products.find((product) => product.product_Id === productId).status : undefined;
-  const countryStatus = countryId ? countries.find((country) => country.country_Id === countryId).status : undefined;
-  const stateStatus = stateId ? states.find((state) => state.state_Id === stateId).status : undefined;
+function onStateChange(evt) {
+  selected.state = fetchedData.states.find(
+    (state) => state.state_id === evt.target.value
+  );
+  checkShippable();
+}
 
-  if(productStatus){
-    if(countryStatus && countryStatus !== productStatus) return false;
-    if(stateStatus && stateStatus !== productStatus) return false;
+function onProductChange(evt) {
+  selected.product = fetchedData.products.find(
+    (product) => product.product_id === evt.target.value
+  );
+  checkShippable();
+}
+
+function checkShippable() {
+  const { country, state, product } = selected;
+  const errorSpan = document.querySelector('#error');
+  if (product) {
+    if (country && country.country_status !== product.product_status) {
+      errorSpan.setAttribute('style', 'display:inline; background-color: red;');
+      return false;
+    }
+    if (state && state.state_status !== product.product_status) {
+      errorSpan.setAttribute('style', 'display:inline; background-color: red;');
+      return false;
+    }
   }
+  errorSpan.setAttribute('style', 'display:none; background-color: red;');
   return true;
 }
 
